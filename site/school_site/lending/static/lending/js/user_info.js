66d69d0f5ca7eeff76b8606652bcec5e68d6e4cb
tu60rk <tu60rk@yandex.ru>
2022-07-02 23:25:47 +0300

    realese 1.0

--- site/school_site/lending/static/lending/js/user_info.js
+++ site/school_site/lending/static/lending/js/user_info.js
@@ -7,20 +7,27 @@ function create_element(tag, name, value){
 };
 
 function get_user_info(){
-    //$.get("https://ipinfo.io", function (response) {
+    $.get("https://ipinfo.io", function (response) {
+        // let user_info = [
+        //     'city..' + 'izhniy Novgorod',//response.city,
+        //     'country..' + 'RU',//response.country,
+        //     'ip..' + '37.114.28.16',//response.ip,
+        //     'loc..' + '56.3287,44.0020',//response.loc,
+        //     'region..' + 'Nizhny Novgorod Oblast'//response.region,
+        // ];
         let user_info = [
-            'city..' + 'izhniy Novgorod',//response.city,
-            'country..' + 'RU',//response.country,
-            'ip..' + '37.114.28.16',//response.ip,
-            'loc..' + '56.3287,44.0020',//response.loc,
-            'region..' + 'Nizhny Novgorod Oblast'//response.region,
+            'city..' + response.city,
+            'country..' + response.country,
+            'ip..' + response.ip,
+            'loc..' + response.loc,
+            'region..' + response.region,
         ];
-        console.log(user_info);
+        //console.log(user_info);
         for (let user of user_info){
             let name = user.split('..')[0];
             let value = user.split('..')[1];
             document.getElementById('info-user-first').appendChild(create_element('input', name, value));
             document.getElementById('info-user-second').appendChild(create_element('input', name, value));
         };
-    //}, "jsonp")
+    }, "jsonp")
 };
